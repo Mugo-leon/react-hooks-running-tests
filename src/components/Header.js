d92fd2d0826e4852660e2f57edf32438d38dcf9e
Leon <leon.muriuki@student.moringaschool.com>
2023-11-16 21:30:34 +0300

    Completed Assignment

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,9 @@
 import React from "react";
-
 function Header(props) {
       return (
         <h1>
           {/* uncomment the line below to pass the test */}
-          {/* hello from the Header! */}
+          <p>hello from the Header!</p>
         </h1>
       )
 }
